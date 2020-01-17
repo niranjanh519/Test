ff8cc2a6df83a550c89ec11d2da3d73b9200e465
Cloud User <ec2-user@ip-172-31-47-55.ap-south-1.compute.internal>
2020-01-17 20:30:32 +0000

    commiting nickbranch

---
+++ nick/JavaDeveloper.java
@@ -0,0 +1,19 @@
+//Shridhar
+
+package com.nick;
+
+import java.io.PrintStream;
+
+public class JavaDeveloper implements Developer {
+
+	private PrintStream out;
+
+	public JavaDeveloper(PrintStream out){
+		this.out = out;
+	}
+
+	public void doStuff(){
+		out.println("I am developing with Java.");
+	}
+
+}
