6f2943c64372f3d1235a267bef0ea99a466ceef7
Cloud User <ec2-user@ip-172-31-16-38.us-east-2.compute.internal>
2020-01-17 15:26:03 +0000

    files added

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello
+ {
+	private String name;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void printHello() {
+		System.out.println("Hello ! " + name);
+	}
+}
